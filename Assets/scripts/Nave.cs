c3b044fcd83ec459ee19b815c478cb82d6f07b30
Ely Galdino dos Santos Junior <ext.ely.junior@dafiti.com.br>
2019-12-04 13:59:51 -0300

    Add router

--- Assets/scripts/Nave.cs
+++ Assets/scripts/Nave.cs
@@ -7,9 +7,9 @@ public class Nave : MonoBehaviour
     public bool selected = false;
     Rigidbody2D rb;
     Vector2 mousepos;
-    float rotation = 200f;
-    float speed = 2f;
-   public bool move = false;
+    public float rotation = 500f;
+    public float speed = 3f;
+    public bool move = false;
     void Start()
     {
         rb = this.GetComponent<Rigidbody2D>();
@@ -31,28 +31,43 @@ public class Nave : MonoBehaviour
                 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
                 move = true;
             }
-            if (move) { 
+            
+        }
+        if (move) { 
                 Vector2 pointarget = (Vector2)transform.position - (Vector2)mousepos;
 
                 pointarget.Normalize();
+                
 
                 float value = Vector3.Cross(pointarget, transform.up).z;
 
                 rb.angularVelocity = rotation * value;
 
                 rb.velocity = transform.up * speed;
-                Debug.Log(Vector3.Distance(mousepos, transform.position));
+                // Debug.Log(Vector3.Distance(mousepos, transform.position));
 
-                if (Vector3.Distance(mousepos, transform.position) < 0.5)
+                if (Vector3.Distance(mousepos, transform.position) < 0.1)
                 {
                     move = false;
                     rb.velocity = transform.up * 0f;
                     rb.angularVelocity = rotation * 0f;
                 }
-               
-
-
             }
+    }
+     private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("nave")){
+                move = false;
+                rb.velocity = transform.up * 0f;
+                rb.angularVelocity = rotation * 0f;
+            
         }
     }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        // if (collision.CompareTag("nave")){
+        //         move = true;
+        // }
+    }
+
 }
---
+++ Assets/scripts/Router.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Router : MonoBehaviour
+{
+    bool active = false;
+    Vector2 mousepos;
+    LineRenderer lineRenderer;
+    public GameObject nave;
+    void Start()
+    {
+        lineRenderer = this.gameObject.GetComponent<LineRenderer>();
+        lineRenderer.SetPosition(0, new Vector3(0f, 0f));
+        lineRenderer.SetPosition(1, new Vector3(0f, 0f));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(nave.GetComponent<Nave>().selected){
+            if (Input.GetKeyDown(KeyCode.Mouse1)) {
+                mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+                active=true;
+            }
+        }     
+
+        if(active){   
+            lineRenderer.SetPosition(0, new Vector3(nave.transform.position.x, nave.transform.position.y));
+            lineRenderer.SetPosition(1, new Vector3(mousepos.x, mousepos.y));
+        }
+    }
+}
